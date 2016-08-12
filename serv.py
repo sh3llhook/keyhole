9e6cd333699b59c2d2085f7340b418a3fed8378e
sh3llhook <sean00mcc@gmail.com>
2016-08-11 22:48:45 -0400

    init

---
+++ serv.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+import socket
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('localhost', 10000)
+print >>sys.stderr, 'starting up on %s port %s' % server_address
+sock.bind(server_address)
+
+sock.listen(1)
+
+while True:
+    # Wait for a connection
+    print >>sys.stderr, 'waiting for a connection'
+    connection, client_address = sock.accept()
+    try:
+        print >>sys.stderr, 'connection from', client_address
+
+        # Receive the data in small chunks and retransmit it:
+        data = connection.recv(256)
+        print >>sys.stderr, 'received "%s"' % data
+
+    finally:
+        # Clean up the connection
+        connection.close()
