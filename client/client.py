66573db5f30becd2e6172fa4607596a0d0ad888f
sh3llhook <sean00mcc@gmail.com>
2016-08-12 15:21:52 -0400

    can create a new uesr, get a token for an existing user, and can send the request for a new record. Server does not actually store it yet but that's the serfver problem.

---
+++ client/client.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+# create a new user
+def create_user():
+    url = 'http://127.0.0.1:5000/api/users'
+    username = raw_input("[+] Enter username: ")
+    password = raw_input("[+] Enter password: ")
+    response = requests.post(url, json={"username":username, "password":password})
+    print response.status_code
+
+
+# get token for your user. This allows us to make authorized requests without sending
+# creds all over the place.
+def get_token():
+    url = 'http://127.0.0.1:5000/api/token'
+    username = raw_input("[+] Enter your username: ")
+    password = raw_input("[+] Enter your password: ")    
+
+    print "[+] Fetching token... "
+
+    response = requests.get(url, auth=HTTPBasicAuth(username, password), verify=True)
+    
+    if(response.ok):
+        jData = json.loads(response.content)
+        user_token = jData['token']
+        print "[+] Token:", jData['token']
+    else: print "[!] ERROR!: ", response.status_code
+    return user_token
+
+
+# create new record
+def create_record(user_token):
+    url = 'http://127.0.0.1:5000/api/records/new'
+    print '[+] Enter the values below to add a new record to the database...'
+
+    ip = raw_input('[+] IP Address: ')
+    uname = raw_input('[+] Username: ')
+    key = raw_input('[+] SSH key: ')
+    passw = raw_input('[+] Password: ')
+    comments = raw_input('[+] Comments: ')
+
+    response = requests.post(url, auth=HTTPBasicAuth(user_token, 'x'), json={'ip':ip, 'uname':uname, 'key':key, 'passw':passw, 'comments':comments})
+
+    print response.status_code
+
+
+
+if __name__ == '__main__':
+    while True:
+        uinput = raw_input("What would you like to do? ")
+        if uinput == "create user":
+            create_user()
+        elif uinput == "get token":
+            token = get_token()
+        elif uinput == "create record":
+            print token
+            create_record(token)
+        else:
+            print "blah you're wrong"
